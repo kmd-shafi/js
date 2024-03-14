788597c79844056e39e813ad67f5f2f4c897e38b
kmd-shafi <154796878+kmd-shafi@users.noreply.github.com>
2024-03-14 13:56:01 +0000

    math&number

---
+++ 10_nums_and_maths/maths.js
@@ -0,0 +1,22 @@
+console.log(Math);//=>36 methods or funtions
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.6));
+console.log(Math.min(1,2,3,4,5,6,7,8));
+console.log(Math.max(1,2,3,4,5,6,7,8));
+console.log(Math.random());//=>0 to 1
+console.log((Math.random())*10+1);//=>adding one because zero also come 
+
+console.log(Math.floor(Math.random()*10)+1);
+
+//formula 10to 20 random number ex=11,12,12,...to 20
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min)+1)+min);
+
+
+
---
+++ 10_nums_and_maths/nums.js
@@ -0,0 +1,18 @@
+const score=400
+console.log(score);
+
+
+const balance =new Number(100); //=>7 methods or funtiond (prototypes)
+console.log(balance)
+
+
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+
+const otherNumber=23.5578
+
+console.log(otherNumber.toPrecision(3));
+
+
+const hundrads=1000000
+console.log(hundrads.toLocaleString('en-IN'));
