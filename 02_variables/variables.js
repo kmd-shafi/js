94cfaef3d644fea62b1a78c77b2dcb1d2501cbd2
kmd-shafi <154796878+kmd-shafi@users.noreply.github.com>
2024-03-14 07:21:39 +0000

    variables and datatypes

---
+++ 02_variables/variables.js
@@ -0,0 +1,19 @@
+const accountId=11445
+let accountEmail="shaikshafi5259@gmail.com"
+var accountPassword="12345"
+accountCity="ananthapur"
+let accountState;
+
+// accountId=2
+
+accountEmail="@shafi"
+accountPassword="1234"
+accountCity="Bengaluru"
+
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity.accountState])
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope 
+*/
---
+++ 03_dataTypes/dataTypes.js
@@ -0,0 +1,39 @@
+"use strict"; // treat all js code as a newer vertion
+
+//alert(3+3) we are using node.js, not browser
+console.log(3+3)
+console.log("shafi"); // code readability should high
+
+// documentation tc39 or mdn
+
+let name ="shafi" //string
+let age=18 //number
+let isLoggedIn=false //Boolean 
+
+// #primitive data types
+
+//1 number=>2 to 53
+//bigint=>big number
+
+//2 string=>"",''
+
+//3 boolean=>true/false
+
+//4 null=>standalone value
+
+//5 undefine=>
+
+//6 symbol=>unique 
+
+
+//object 
+
+
+console.log(typeof "shafi")
+
+console.log(typeof age);
+
+console.log(typeof null); // object 🚀
+
+console.log((typeof undefined)); //undefined
+
