6abd76adcd3d61deeadc8a13c16f2e85ada0e941
kmd-shafi <154796878+kmd-shafi@users.noreply.github.com>
2024-03-14 10:14:46 +0000

    dataTypeSummary

---
+++ 07_dataTypes_summary/dataTypes_summary.js
@@ -0,0 +1,23 @@
+// # primitive -7types(strings-"",numbers-5,Boolean-true,null-empty,undefind-let name;,symbol-uniq-id's,BigInt-bignumber-14525789965n)
+//# (non primitive or reference)-Array,objects,functions
+//#JavaScript is a (dynamically typed language), which means that data types of variables are determined by the value ex-
+// const score=100 i am not definding this is a nember in side the score
+
+
+
+// Array
+const heros=["shafi","shabana","basha","ayesha"];
+
+// Object-key:vale 
+let obj={
+name:'shafi',
+age:25,
+}
+//funtion
+const myfuntion=function(){
+    console.log("hello world")
+}
+console.log(typeof heros) //=>object 
+console.log(typeof myfuntion) // funtion object 
+console.log(typeof heros) //object 
+
