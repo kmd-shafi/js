766213e063d5f48f890b233a8f8414778b0e7502
kmd-shafi <154796878+kmd-shafi@users.noreply.github.com>
2024-03-16 04:19:49 +0000

    date

---
+++ 11_dates/dates.js
@@ -0,0 +1,22 @@
+//beginning of january1,1970
+
+// let myDate=new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// let myCreatedDate=new Date(2024,2,14,7,45)=> 2024/mar/14, 7:45:00:pm
+// let myCreatedDate=new Date(2024,2,14,)//=>2024/mar/14
+// let myCreatedDate=new Date("2024-01-14")//=>(yy/mm/dd)
+// let myCreatedDate=new Date("03-16-2024")//=>(mm/dd/yy)
+// console.log(myCreatedDate.toString());
+
+
+// let myTimeStamp= Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));//=>seconds' 
+
+
+// let newDate=new Date();
+// console.log(newDate.getDay());
